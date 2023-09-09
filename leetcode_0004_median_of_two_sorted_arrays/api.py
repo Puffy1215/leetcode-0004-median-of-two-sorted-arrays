37c1c4b3284fc91ecfb678c7555e680fc3431941
Andy Xie <andy.xie.opensource@gmail.com>
2023-09-09 19:08:50 +0800

    api.py: Add types

--- leetcode_0004_median_of_two_sorted_arrays/api.py
+++ leetcode_0004_median_of_two_sorted_arrays/api.py
@@ -1,10 +1,10 @@
 """API for solving problem Median of Two Sorted Arrays"""
 
-def _check_preconditions(...) -> bool:
+def _check_preconditions(nums1: list[int], nums2: list[int]) -> bool:
     pass
 
 
-def median_of_two_sorted_arrays(...) -> ...:
+def median_of_two_sorted_arrays(nums1: list[int], nums2: list[int]) -> int:
     """Solves problem Median of Two Sorted Arrays"""
 
     assert _check_preconditions(...)
