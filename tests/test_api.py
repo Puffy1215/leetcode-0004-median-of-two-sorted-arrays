7223eb6ad534ef1af5419025bc0b3ceb452ec143
Andy Xie <andy.xie.opensource@gmail.com>
2023-09-09 22:09:39 +0800

    test_api.py: Add types

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,13 +6,13 @@ from leetcode_0004_median_of_two_sorted_arrays import api
 
 
 @pytest.mark.parametrize(
-    ["result", ...],
+    ["result", "nums1", "nums2"],
     (
         [..., ...],
         [..., ...],
     )
 )
-def test_median_of_two_sorted_arrays(result, ...) -> None:
+def test_median_of_two_sorted_arrays(result: int, nums1: list[int], nums2: list[int]) -> None:
     """Tests solution for problem Median of Two Sorted Arrays"""
 
-    assert api.median_of_two_sorted_arrays(...) == result
+    assert api.median_of_two_sorted_arrays(nums1, nums2) == result
