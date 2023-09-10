ada92981762eaeafe8b488bcdbb5e387f9e5123c
Andy Xie <andy.xie.opensource@gmail.com>
2023-09-10 21:12:50 +0800

    api.py: Implement _check_preconditions

--- leetcode_0004_median_of_two_sorted_arrays/api.py
+++ leetcode_0004_median_of_two_sorted_arrays/api.py
@@ -1,7 +1,27 @@
 """API for solving problem Median of Two Sorted Arrays"""
 
+LEN_MAX = 1000
+LEN_MIN = 0
+
+NUM_MAX = 10**6
+NUM_MIN = -NUM_MAX
+
 def _check_preconditions(nums1: list[int], nums2: list[int]) -> bool:
-    pass
+    for nums in [nums1, nums2]:
+        if not sorted(nums):
+            return False
+        
+        if not LEN_MIN <= len(nums) <= LEN_MAX:
+            return False
+        
+        for x in nums:
+            if not NUM_MIN <= x <= NUM_MAX:
+                return False
+    
+    if not LEN_MIN + 1 <= len(nums1) + len(nums2) <= 2*LEN_MAX:
+        return False
+    
+    return True
 
 
 def median_of_two_sorted_arrays(nums1: list[int], nums2: list[int]) -> int:
