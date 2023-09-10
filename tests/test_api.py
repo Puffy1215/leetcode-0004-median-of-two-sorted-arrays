224cf37a0346e98e3ef5a6710e6597d8abef2098
Andy Xie <andy.xie.opensource@gmail.com>
2023-09-10 20:10:05 +0800

    test_api.py: Add data for test

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,8 +8,8 @@ from leetcode_0004_median_of_two_sorted_arrays import api
 @pytest.mark.parametrize(
     ["result", "nums1", "nums2"],
     (
-        [..., ...],
-        [..., ...],
+        [2, [1,3], [2]],
+        [2.5, [1,2], [3,4]],
     )
 )
 def test_median_of_two_sorted_arrays(result: int, nums1: list[int], nums2: list[int]) -> None:
